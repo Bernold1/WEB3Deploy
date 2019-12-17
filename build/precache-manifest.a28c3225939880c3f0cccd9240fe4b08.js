b3fd84e32d949656ad29af4682944c7502478bf6
Christopher Bernold <christopherbernold@icloud.com>
2019-12-17 18:54:06 +0100

    new build

---
+++ build/precache-manifest.a28c3225939880c3f0cccd9240fe4b08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73a990f5af2157c3ea990dd78ec46e75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d45bf8a150381929f9b6",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "c7ce4e4ace2d0fd1ad3e",
+    "url": "/static/css/main.47300d44.chunk.css"
+  },
+  {
+    "revision": "d45bf8a150381929f9b6",
+    "url": "/static/js/2.83d987d9.chunk.js"
+  },
+  {
+    "revision": "53cf24413b0948c556e7c9d5a0747ea5",
+    "url": "/static/js/2.83d987d9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c7ce4e4ace2d0fd1ad3e",
+    "url": "/static/js/main.319d90fa.chunk.js"
+  },
+  {
+    "revision": "995115191374ffa5a616",
+    "url": "/static/js/runtime-main.bbdefcd8.js"
+  }
+]);
